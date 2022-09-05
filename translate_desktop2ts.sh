b836a498b8e783e0dff3820302957f15ee8416eb
rewine <luhongxu@uniontech.com>
2022-09-05 15:21:05 +0800

    chore: don't use hardcode path
    Log: don't use hardcode path

--- translate_desktop2ts.sh
+++ translate_desktop2ts.sh
@@ -3,4 +3,4 @@
 DESKTOP_SOURCE_FILE=dde-device-formatter.desktop
 DESKTOP_TS_DIR=translations/dde-device-formatter-desktop/
 
-/usr/bin/deepin-desktop-ts-convert desktop2ts $DESKTOP_SOURCE_FILE $DESKTOP_TS_DIR
+deepin-desktop-ts-convert desktop2ts $DESKTOP_SOURCE_FILE $DESKTOP_TS_DIR
