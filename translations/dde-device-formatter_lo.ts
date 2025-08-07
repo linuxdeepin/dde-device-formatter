870cb8d98adaac00f53c62701b41033fed8ceeee
Tian Shilin <tianshilin@uniontech.com>
2025-08-07 18:43:16 +0800

    feat: Update translations for Lao locale
    log: Translation update

---
+++ translations/dde-device-formatter_lo.ts
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lo">
+<context>
+    <name>FinishPage</name>
+    <message>
+        <location filename="../view/finishpage.cpp" line="22"/>
+        <source>Format successful</source>
+        <translation>การจัดรูปแบบสำเร็จแล้ว</translation>
+    </message>
+</context>
+<context>
+    <name>FormatingPage</name>
+    <message>
+        <location filename="../view/formatingpage.cpp" line="29"/>
+        <source>Formatting the disk, please wait...</source>
+        <translation>กำลังจัดรูปแบบฮาร์ดดิสก์ โปรดรอ...</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../view/mainpage.cpp" line="99"/>
+        <source>Type</source>
+        <translation>ชนิด</translation>
+    </message>
+    <message>
+        <location filename="../view/mainpage.cpp" line="117"/>
+        <source>Label</source>
+        <translation>ป้ายกำกับ</translation>
+    </message>
+    <message>
+        <location filename="../view/mainpage.cpp" line="127"/>
+        <source>Quick Format</source>
+        <translation>จัดรูปแบบเร็ว</translation>
+    </message>
+    <message>
+        <location filename="../view/mainpage.cpp" line="144"/>
+        <source>Formatting will erase all data on the disk.</source>
+        <translation>การจัดรูปแบบนี้จะล้างข้อมูลทั้งหมดจากฮาร์ดดิสก์</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="68"/>
+        <source>Format</source>
+        <translation>จัดรูปแบบ</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="213"/>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="218"/>
+        <source>Formatting...</source>
+        <translation>กำลังจัดรูปแบบ...</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="244"/>
+        <source>Done</source>
+        <translation>เสร็จสิ้น</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="254"/>
+        <source>Quit</source>
+        <translation>ออกจาก</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="255"/>
+        <source>Your disk has been removed</source>
+        <translation>ฮาร์ดดิสก์ของคุณถูกถอดออกแล้ว</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="258"/>
+        <source>Failed to format the device</source>
+        <translation>ไม่สามารถจัดรูปแบบอุปกรณ์ได้</translation>
+    </message>
+    <message>
+        <location filename="../view/mainwindow.cpp" line="259"/>
+        <source>Reformat</source>
+        <translation>จัดรูปแบบอีกครั้ง</translation>
+    </message>
+</context>
+<context>
+    <name>MessageDialog</name>
+    <message>
+        <location filename="../dialogs/messagedialog.cpp" line="23"/>
+        <source>OK</source>
+        <translation>ตกลง</translation>
+    </message>
+</context>
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="38"/>
+        <source>System Disk</source>
+        <translation>ລាស់ដុស්කូ</translation>
+    </message>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="42"/>
+        <source>%1 Encrypted</source>
+        <translation>%1 ཀំណត់ໄດ้</translation>
+    </message>
+    <message>
+        <location filename="../utils/udisksutils.cpp" line="44"/>
+        <source>%1 Volume</source>
+        <translation>%1 ཀាន័រිය</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="68"/>
+        <source>dde device formatter</source>
+        <translation>dde དុស්කូរා</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="84"/>
+        <source>Device does not exist</source>
+        <translation>ອື້ອງປະສິດທິພາບບໍ່ມີ</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="93"/>
+        <source>The device is read-only</source>
+        <translation>ອື້ອງແມ່ນໍ່ພຸ່ນໄດ້</translation>
+    </message>
+</context>
+<context>
+    <name>WarnPage</name>
+    <message>
+        <location filename="../view/warnpage.cpp" line="23"/>
+        <source>Formatting will erase all data on this disk, are you sure you want to continue? It cannot be restored.</source>
+        <translation>ການຮຸບຮື້ານຈະລູກທັນທີ້ທັງໍ່ຂໍ້ນ, ད້ານທີ້ນເຊື່ອວ່າທ່ານຕ້ອງການທື້ນໄດ້ບໍ? དྷີ້ນບໍ່ສາມາດຖືກກັ້ງຄັ້ງອື້ອງໄດ້.</translation>
+    </message>
+</context>
+</TS>
